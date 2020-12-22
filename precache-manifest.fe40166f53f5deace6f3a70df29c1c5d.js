e237b67bfaecb77498a83f0727616f991f440cb3
molly soufrine <mollysoufrine@gmail.com>
2020-12-22 16:54:05 -0500

    Updates

---
+++ precache-manifest.fe40166f53f5deace6f3a70df29c1c5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44fb3628ea7d906c10c812af638b6ac1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6ae6ccec2a0c87fbf10d",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e62e1305792e1f3dc0a",
+    "url": "/portfolio/static/js/2.de08e2ef.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.de08e2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ae6ccec2a0c87fbf10d",
+    "url": "/portfolio/static/js/main.a0766076.chunk.js"
+  },
+  {
+    "revision": "2f727e1dc7e6ccb61813",
+    "url": "/portfolio/static/js/runtime-main.d05f8178.js"
+  },
+  {
+    "revision": "843743c881617914910653efc728b6cb",
+    "url": "/portfolio/static/media/molly.843743c8.jpg"
+  }
+]);
